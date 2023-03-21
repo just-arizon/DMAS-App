d4a2e11704d1663da6f79551e0002046c9e61921
Arinze <calistusarinzechinweuba@gmail.com>
2023-03-21 03:19:44 +0100

    Add hamburger menu

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const refreshBtn = document.getElementById("refresh-btn");
- const userInputOne = document.querySelector("#inp-one");
-    const userInputTwo = document.querySelector("#inp-two");
+const userInputOne = document.querySelector("#inp-one");
+const userInputTwo = document.querySelector("#inp-two");
 const submitToCheck = document.querySelector(".custom-btn");
 
 const listItems = document.querySelectorAll("#myList li");
+const secondListItems = document.querySelectorAll("#myHamburgerMenu li");
 const outputDiv = document.querySelector("#operator");
 
+const btn = document.querySelector(".hamburger");
+const navMenu = document.querySelector("#myHamburgerMenu")
+
+btn.addEventListener("click", () => {
+  navMenu.classList.toggle("active")
+})
 listItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    outputDiv.textContent = item.textContent;
+    if (item.textContent == "Addition") {
+      outputDiv.textContent = "+";
+    }
+    if (item.textContent == "Subtraction") {
+      outputDiv.textContent = "-";
+    }
+    if (item.textContent == "Multiplying") {
+      outputDiv.textContent = "*";
+    }
+    if (item.textContent == "Division") {
+      outputDiv.textContent = "/";
+    }
+  });
+});
+secondListItems.forEach((item) => {
   item.addEventListener("click", () => {
     outputDiv.textContent = item.textContent;
     if (item.textContent == "Addition") {
@@ -28,32 +52,45 @@ submitToCheck.addEventListener("click", (event) => {
   event.preventDefault(); // prevent the default form submission behavior
 
   if (outputDiv.textContent === "+") {
-    function Addiition(num1,num2){
-        var sum = Number(num1) + Number(num2);
-        return sum
+    function Addiition(num1, num2) {
+      var sum = Number(num1) + Number(num2);
+      return sum;
     }
-    document.querySelector("#demo").innerHTML = Addiition(userInputOne.value,userInputTwo.value)
+    document.querySelector("#demo").innerHTML = Addiition(
+      userInputOne.value,
+      userInputTwo.value
+    );
   }
   if (outputDiv.textContent === "-") {
-    function Subtraction(num1,num2){
-        var minus = Number(num1) - Number(num2);
-        return minus
+    function Subtraction(num1, num2) {
+      var minus = Number(num1) - Number(num2);
+      return minus;
     }
-    document.querySelector("#demo").innerHTML = Subtraction(userInputOne.value,userInputTwo.value)
+    document.querySelector("#demo").innerHTML = Subtraction(
+      userInputOne.value,
+      userInputTwo.value
+    );
   }
   if (outputDiv.textContent === "*") {
-    function Multiplication(num1,num2){
-        var multiply = Number(num1) * Number(num2);
-        return multiply
+    function Multiplication(num1, num2) {
+      var multiply = num1 * num2;
+      return multiply;
     }
-    document.querySelector("#demo").innerHTML = Multiplication(userInputOne.value,userInputTwo.value)
+
+    document.querySelector("#demo").innerHTML = Multiplication(
+      userInputOne.value,
+      userInputTwo.value
+    );
   }
   if (outputDiv.textContent === "/") {
-    function Division(num1,num2){
-        var divide = Number(num1) / Number(num2);
-        return divide
+    function Division(num1, num2) {
+      var divide = Number(num1) / Number(num2);
+      return divide;
     }
-    document.querySelector("#demo").innerHTML = Division(userInputOne.value,userInputTwo.value)
+    document.querySelector("#demo").innerHTML = Division(
+      userInputOne.value,
+      userInputTwo.value
+    );
   }
 });
 
